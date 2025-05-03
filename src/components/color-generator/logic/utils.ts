4e39aec58b1fa560096fb870f49a7f15f447e07c
Citydayz <bonjour@hugo-scheer.fr>
2025-05-03 18:39:20 +0200

    Rapid fix 2

--- src/components/color-generator/logic/utils.ts
+++ src/components/color-generator/logic/utils.ts
@@ -1,5 +1,5 @@
 import chroma from "chroma-js";
-import type { Swatch } from "./types";
+import type { Swatch } from "../types";
 
 export const harmonyOptions = {
   analogues: [0, -10, 10, 30, -30],
